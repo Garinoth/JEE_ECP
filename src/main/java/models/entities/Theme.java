38489166a79319f013d23d330922530e8a725545
Garinoth <cdavidrp@gmail.com>
2015-03-05 11:24:45 +0100

    ADD entity Theme

---
+++ src/main/java/models/entities/Theme.java
@@ -0,0 +1,54 @@
+package models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Theme {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+    private String name;
+    
+    private String question;
+    
+    private Double rating;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Theme [id=" + id + ", name=" + name + ", question=" + question
+				+ ", rating=" + rating + "]";
+	}
+    
+}
