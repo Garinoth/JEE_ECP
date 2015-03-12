978b2bb198d703f94de789d3a6490281f087596b
Garinoth <cdavidrp@gmail.com>
2015-03-12 12:51:44 +0100

    UPDATE DaoJpaFactory

--- src/main/java/models/daos/jpa/DaoJpaFactory.java
+++ src/main/java/models/daos/jpa/DaoJpaFactory.java
@@ -1,5 +1,8 @@
 package models.daos.jpa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -8,19 +11,30 @@ import models.daos.ThemeDao;
 import models.daos.VoteDao;
 
 import org.apache.logging.log4j.LogManager;
+import org.eclipse.persistence.config.PersistenceUnitProperties;
 
 public class DaoJpaFactory extends DaoFactory {
-    private static final String PERSISTENCE_UNIT = "tictactoe";
+    private static final String PERSISTENCE_UNIT = "JEE_ECP";
 
-    private static EntityManagerFactory entityManagerFactory = Persistence
-            .createEntityManagerFactory(PERSISTENCE_UNIT);
-
+    private static EntityManagerFactory entityManagerFactory;
+    
     public DaoJpaFactory() {
-        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
     }
 
     public static EntityManagerFactory getEntityManagerFactory() {
+        if (entityManagerFactory == null) {
+            entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+            LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
+        }
         return entityManagerFactory;
+    }
+    
+    public static void prepareFactoryWithDropAndCreateTables() {
+        Map<String, String> properties = new HashMap<>();
+        properties.put(PersistenceUnitProperties.DDL_GENERATION,
+                PersistenceUnitProperties.DROP_AND_CREATE);
+        entityManagerFactory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, properties);
+        LogManager.getLogger(DaoJpaFactory.class).debug("create Entity Manager Factory");
     }
 
 	@Override
