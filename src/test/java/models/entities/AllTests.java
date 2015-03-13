60eb3f6ec66cab32fdfb84f4a9d6c12a15c73564
Garinoth <cdavidrp@gmail.com>
2015-03-13 17:31:31 +0100

    ADD AllTests test suite for entities

---
+++ src/test/java/models/entities/AllTests.java
@@ -0,0 +1,11 @@
+package models.entities;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ThemeTest.class, VoteTest.class })
+public class AllTests {
+
+}
