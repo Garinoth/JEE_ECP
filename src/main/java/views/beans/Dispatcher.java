141897ccf653c68154cc32ed8c14589ba20b40d1
Garinoth <cdavidrp@gmail.com>
2015-03-19 20:31:24 +0100

    UPDATE Dispatcher to handle post calls to addTheme

--- src/main/java/views/beans/Dispatcher.java
+++ src/main/java/views/beans/Dispatcher.java
@@ -8,70 +8,77 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.daos.DaoFactory;
+import models.daos.jpa.DaoJpaFactory;
+
 @WebServlet("/jsp/*")
 public class Dispatcher extends HttpServlet {
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private static String PATH_ROOT_VIEW = "/viewsJsp/";
+	private static String PATH_ROOT_VIEW = "/viewsJsp/";
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		String action = request.getPathInfo().substring(1);
 
-        String action = request.getPathInfo().substring(1);
+		String view;
+		switch (action) {
+		case "themes":
+			ThemesView themesView = new ThemesView();
+			request.setAttribute(action, themesView);
+			view = action;
+			break;
+		case "addTheme":
+			AddThemeView addThemeView = new AddThemeView();
+			request.setAttribute(action, addThemeView);
+			view = action;
+			break;
+		case "votes":
+			VotesView votesView = new VotesView();
+			request.setAttribute(action, votesView);
+			view = action;
+			break;
+		default:
+			view = "home";
+		}
 
-        String view;
-        switch (action) {
-        case "themes":
-            ThemesView themesView = new ThemesView();
-            request.setAttribute(action, themesView);
-            view = action;
-            break;
-        case "addTheme":
-            AddThemeView addThemeView= new AddThemeView();
-            request.setAttribute(action, addThemeView);
-            view = action;
-            break;
-        case "votesView":
-            VotesView votesView= new VotesView();
-            request.setAttribute(action, votesView);
-            view = action;
-            break;
-        default:
-            view = "home";
-        }
+		this.getServletContext()
+				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+				.forward(request, response);
 
-        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-                .forward(request, response);
+	}
 
-    }
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		DaoFactory.setFactory(new DaoJpaFactory());
+		String action = request.getPathInfo().substring(1);
+		String view = "home";
+		switch (action) {
+		case "themes":
+			ThemesView themesView = new ThemesView();
+			request.setAttribute(action, themesView);
+			view = action;
+			break;
+		case "addTheme":
+			AddThemeView addThemeView = new AddThemeView();
+			addThemeView.setName(request.getParameter("name"));
+			addThemeView.setQuestion(request.getParameter("question"));
+			request.setAttribute(action, addThemeView);
+			view = addThemeView.process();
+			break;
+		case "votes":
+			VotesView votesView = new VotesView();
+			request.setAttribute(action, votesView);
+			view = action;
+			break;
+		}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String action = request.getPathInfo().substring(1);
-        String view = "home";
-//        switch (action) {
-//        case "persona":
-//            Persona persona = new Persona();
-//            persona.setId(Integer.valueOf(request.getParameter("id")));
-//            persona.setNombre(request.getParameter("nombre"));
-//            persona.setRol(request.getParameter("rol"));
-//            PersonaView personaView = new PersonaView();
-//            personaView.setPersona(persona);
-//            request.setAttribute(action, personaView);
-//            view = personaView.process();
-//            break;
-//        case "rol":
-//            RolView rolView = new RolView();
-//            rolView.setRol(request.getParameter("rol"));
-//            request.setAttribute(action, rolView);
-//            view = rolView.process();
-//            break;
-//        }
-
-        this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-                .forward(request, response);
-    }
+		this.getServletContext()
+				.getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
+				.forward(request, response);
+	}
 
 }
