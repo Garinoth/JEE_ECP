40e9dc077e292550d5d061ce7d3fc41b66ad8780
Garinoth <cdavidrp@gmail.com>
2015-03-29 17:30:45 +0200

    UPDATE DeleteThemeView to work with the new jsf

--- src/main/java/views/beans/DeleteThemeView.java
+++ src/main/java/views/beans/DeleteThemeView.java
@@ -1,13 +1,26 @@
 package views.beans;
 
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
 import models.daos.jpa.DaoJpaFactory;
 import models.entities.Theme;
 import controllers.ejb.ControllerEjbFactory;
 
+@ManagedBean(name="deleteTheme")
 public class DeleteThemeView {
 	
 	private Integer id;
+	
+	@ManagedProperty(value = "#{themes}")
+	private ThemesView themesView;
 	private Theme theme;
+	
+	@ManagedProperty(value = "auth")
 	private String auth;
 	private static final String AUTH = "666";
 
@@ -35,9 +48,30 @@ public class DeleteThemeView {
 		this.auth = auth;
 	}
 
+	public ThemesView getThemesView() {
+		return themesView;
+	}
+
+	public void setThemesView(ThemesView themesView) {
+		this.themesView = themesView;
+	}
 
 	public void update() {
+		System.out.println(id);
 		this.setTheme(DaoJpaFactory.getFactory().getThemeDao().read(id));
+	}
+	
+	@PostConstruct
+	public void updateJsf() {
+		if (themesView.getThemeId() != null) {
+			this.setId(Integer.valueOf(themesView.getThemeId()));
+		} else if (this.id == null) {
+			Map<String,String> params = 
+	                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+			String id = params.get("id");
+			this.setId(Integer.valueOf(id));
+		}
+		update();
 	}
 	
 	public String process() {
