c20430992f63859141caad8118a8af9b1f330599
Garinoth <cdavidrp@gmail.com>
2015-03-29 21:24:38 +0200

    ADD show votes functionality

--- src/main/java/views/beans/VotesView.java
+++ src/main/java/views/beans/VotesView.java
@@ -1,11 +1,18 @@
 package views.beans;
 
-import controllers.ejb.ControllerEjbFactory;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+
 import models.daos.jpa.DaoJpaFactory;
 import models.entities.Theme;
-import models.entities.Vote;
 import models.utils.Studies;
+import controllers.ejb.ControllerEjbFactory;
 
+@ManagedBean(name="votes")
 public class VotesView {
 
 	private Integer id;
@@ -16,6 +23,9 @@ public class VotesView {
 	private Studies studies;
 	private Studies[] studiesOptions;
 
+	@ManagedProperty(value = "#{themes}")
+	private ThemesView themesView;
+	
 	public VotesView() {
 
 	}
@@ -76,6 +86,14 @@ public class VotesView {
 		this.studiesOptions = studiesOptions;
 	}
 
+	public ThemesView getThemesView() {
+		return themesView;
+	}
+
+	public void setThemesView(ThemesView themesView) {
+		this.themesView = themesView;
+	}
+
 	public void update() {
 		this.setStudiesOptions(Studies.values());
 		this.setTheme(DaoJpaFactory.getFactory().getThemeDao().read(id));
@@ -84,6 +102,23 @@ public class VotesView {
 		if (this.studies != null) {
 			this.setAverageByStudies(ControllerEjbFactory.getFactory().getShowVotesController().getAverageByStudies(theme, this.studies));
 		}
+	}
+	
+	@PostConstruct
+	public void updateJsf() {
+		if (themesView.getThemeId() != null) {
+			this.setId(Integer.valueOf(themesView.getThemeId()));
+		} else if (this.id == null) {
+			Map<String,String> params = 
+	                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+			String id = params.get("id");
+			this.setId(Integer.valueOf(id));
+		}
+		update();
+	}
+	
+	public String process() {
+		return "votes";
 	}
 
 }
