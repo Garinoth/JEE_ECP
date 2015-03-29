8d73b956e47c6cba7f2c083072621ac65cfa0e44
Garinoth <cdavidrp@gmail.com>
2015-03-29 16:18:49 +0200

    Update themes.xhtml and ThemesView to accomodate Deletetheme

--- src/main/java/views/beans/ThemesView.java
+++ src/main/java/views/beans/ThemesView.java
@@ -1,17 +1,22 @@
 package views.beans;
 
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
 
 import models.daos.DaoFactory;
 import models.daos.jpa.DaoJpaFactory;
 import models.entities.Theme;
 
 @ManagedBean(name = "themes")
+@RequestScoped
 public class ThemesView {
-
+	
+	private String themeId;
 	private List<Theme> themes;
 
 	@PostConstruct
@@ -26,6 +31,22 @@ public class ThemesView {
 
 	public void setThemes(List<Theme> themes) {
 		this.themes = themes;
+	}
+	
+	public String getThemeId() {
+		return themeId;
+	}
+
+	public void setThemeId(String themeId) {
+		this.themeId = themeId;
+	}
+
+	public String deleteTheme() {
+		Map<String,String> params = 
+                FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+		String themeId = params.get("themeId");
+		this.setThemeId(themeId);
+		return "deleteTheme";
 	}
 	
 }
